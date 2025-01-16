aae897ea321800e9ab3ddd8987af2750499be02b
Saiban0 <bartched@outlook.fr>
2025-01-16 18:53:20 +0100

    ~ | moved hpps to the bottom of the header

--- cpp_00/ex01/phonebook.h
+++ cpp_00/ex01/phonebook.h
@@ -1,4 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 18:40:25 by bchedru           #+#    #+#             */
+/*   Updated: 2025/01/16 18:34:30 by bchedru          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 
-#include "Contact.hpp"
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include "Contact.hpp"
+#include "Phonebook.hpp"
