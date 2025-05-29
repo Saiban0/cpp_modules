67c9b1c59da78a18ad5953e51ef15a85c0326ce5
Saiban0 <bartched@outlook.fr>
2025-05-29 16:29:23 +0200

    + | completed exercise 4

--- cpp_01/ex04/ft_replace.hpp
+++ cpp_01/ex04/ft_replace.hpp
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 13:15:50 by bchedru           #+#    #+#             */
-/*   Updated: 2025/03/13 14:19:29 by bchedru          ###   ########.fr       */
+/*   Updated: 2025/05/29 15:58:01 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,4 @@
 #include <string>
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -6,16 +6,69 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 13:15:10 by bchedru           #+#    #+#             */
-/*   Updated: 2025/03/13 13:15:10 by bchedru          ###   ########.fr       */
+/*   Updated: 2025/05/29 16:28:45 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_replace.hpp"
 
-int	main(int argc, char *argv)
+std::string	search_and_replace(std::string currentLine, std::string strToFind,
+		std::string replaceStr)
+{
+	size_t	findLen = strToFind.length();
+	for (size_t i = 0; i < currentLine.length(); i++)
+	{
+		if (currentLine[i] == strToFind[0] && !currentLine.compare(i, findLen, strToFind))
+		{
+			currentLine.erase(i, findLen);
+			currentLine.insert(i, replaceStr);
+			i += findLen;
+		}
+	}
+	return (currentLine);
+}
+
+bool	ft_replace(std::fstream& inputFile, std::string fileName,
+		std::string strToFind, std::string replaceStr)
+{
+	std::string		outputName = fileName + ".replace";
+	std::string		currentLine = "";
+	std::fstream	outputFile;
+
+	outputFile.open(outputName.c_str(), std::fstream::out | std::fstream::trunc);
+	if (!outputFile)
+	{
+		std::cout << "Error opening/creating file : " << outputName << std::endl;
+		return (false);
+	}
+	while (std::getline(inputFile, currentLine))
+	{
+		outputFile << std::endl;
+		currentLine = search_and_replace(currentLine, strToFind, replaceStr);
+		outputFile << currentLine;
+	}
+	return (true);
+}
+
+int	main(int argc, char **argv)
 {
 	if (argc != 4)
 	{
 		std::cout << "try ./ex04 <filename> <string to find> <string to replace>" << std::endl;
+		return (1);
 	}
+	else
+	{
+		std::fstream	inputFile;
+		inputFile.open(argv[1], std::fstream::in);
+		if (!inputFile)
+		{
+			std::cout << "Error opening file : " << argv[1] << std::endl;
+			return (1);
+		}
+		if (!ft_replace(inputFile, argv[1], argv[2], argv[3]))
+			return(1);
+		inputFile.close();
+	}
+	return (0);
 }
